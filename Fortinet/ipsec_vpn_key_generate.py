8985dbde15ba79b57de2b0d6fbafe21314da8098
joshbergm <josh.bergman@outlook.com>
2024-03-15 15:26:20 +0100

    initial commit

---
+++ Fortinet/ipsec_vpn_key_generate.py
@@ -0,0 +1,59 @@
+from netmiko import ConnectHandler
+import random
+import string
+import time
+
+## Define variables
+username = "..."
+password = "..."
+local_ip_address = "..."
+remote_ip_address = "..."
+device_type = 'fortinet'
+port = '22'
+key_length = 32
+
+#Specify connection details
+remote_fortigate = {
+    'device_type': device_type,
+    'host': remote_ip_address,
+    'username': username,
+    'password': password,
+    'port': port,
+}
+
+local_fortigate = {
+    'device_type': device_type,
+    'host': local_ip_address,
+    'username': username,
+    'password': password,
+    'port': port,
+}
+
+#Generate PSK
+def generate_key(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in
+    range(length))
+
+VPN_PSK = generate_key(key_length)
+
+#Generate command set
+REMOTE_COMMANDS = ['config vpn ipsec phase1-interface',
+                      'edit "<VPN NAME>"',
+                      'set psk ' + VPN_PSK]
+
+LOCAL_COMMANDS = ['config vpn ipsec phase1-interface',
+                     'edit "<VPN NAME>"',
+                     'set psk ' + VPN_PSK]
+
+#Start SSH connection (remote, local)
+SSH_REMOTE_FORTIGATE = ConnectHandler(**remote_fortigate)
+SSH_LOCAL_FORTIGATE = ConnectHandler(**local_fortigate)
+
+#Send SSH commands (remote, local)
+SSH_REMOTE_FORTIGATE.send_config_set(REMOTE_COMMANDS)
+time.sleep(5)
+SSH_LOCAL_FORTIGATE.send_config_set(LOCAL_COMMANDS)
+
+#Stop SSH connection (remote, local)
+SSH_REMOTE_FORTIGATE.disconnect()
+SSH_LOCAL_FORTIGATE.disconnect()
---
+++ Juniper/push_config_from_file.py
@@ -0,0 +1,91 @@
+##############################################################
+##      - Python script for applying Juniper config         ##
+##      - Author: Joshua Bergman                            ##
+##      - Version: 0.1 (Draft)                              ##
+##      - Date: 15-03-2024                                  ##
+##############################################################
+
+## Import libraries
+import netmiko
+import getpass
+import os
+import sys
+from time import sleep
+from netmiko import ConnectHandler
+
+## Define variables
+username = input("Username: ")
+password = getpass.getpass(prompt="Password: ", stream=None)
+ip_address = input("IP address: ")
+device_type = 'juniper_junos'
+port = '22'
+
+## SSH connection details
+juniper = {
+    'device_type': device_type,
+    'host': ip_address,
+    'username': username,
+    'password': password,
+    'port': port,
+}
+
+## Create session
+net_connect = ConnectHandler(**juniper)
+
+## SSH session succesfull?
+def is_allive(net_connect):
+    null = chr(0)
+
+    try:
+        net_connect.write_channel(null)
+        print("SSH session connected")
+    except (EOFError):
+        print("SSH session denied")
+        sleep(2)
+        sys.exit()
+
+## Import config from file
+pc_username = getpass.getuser()
+config_file_path = os.path.join("c:/Users/", pc_username, "Documents/PythonJuniper/config.txt")
+configuration_file = open(config_file_path)
+
+## Check if file is not empty
+if os.stat(config_file_path).st_size == 0:
+    print("Nothing in file to push")
+    sleep(2)
+    sys.exit()
+
+## Push config
+net_connect.config_mode()
+net_connect.send_config_from_file(config_file_path)
+
+## Ask to view changes
+net_connect.exit_config_mode()
+compare_question = None
+while compare_question not in ("y", "n"):
+    compare_question = input("View changes y/n? ")
+    if compare_question == "y":
+        compare_output = net_connect.send_command('show | compare')
+        print(compare_output)
+    elif compare_question == "n":
+        continue
+    else:
+        print("View changes y/n? ")
+
+## Ask user to commit changes
+net_connect.exit_config_mode()
+commit_question = None
+while commit_question not in ("y", "n"):
+    commit_question = input("Commit changes y/n? ")
+    if commit_question == "y":
+        net_connect.commit()
+    elif commit_question == "n":
+        continue
+    else:
+        print("Commit changes y/n? ")
+
+## Disconnect SSH session
+net_connect.disconnect()
+
+## Print successfull push
+print('Config applied')
