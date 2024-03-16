46816c13d66f1cefe5f7d58ce59f8b8426485392
joshbergm <josh.bergman@outlook.com>
2024-03-16 22:04:53 +0100

    make fortimanager field required

--- Fortinet/03_add_firewall_object_to_fortimanager_adom.py
+++ Fortinet/03_add_firewall_object_to_fortimanager_adom.py
@@ -8,15 +8,40 @@
 import pyFortiManagerAPI
 import getpass
 import sys
-from PyInquirer import prompt
+import prompt
 
 ## Define variables
 username = input("Username: ")
 password = getpass.getpass(prompt="Password: ", stream=None)
-host = input("FortiManager IP: ")
-objectname = input("Object name: ")
-associated_interface = input("Interface (type any for default): ")
-subnet = input("IP address / netmask: ")
+
+## FortiManager Variables
+### Host
+host = ""
+while not host:
+    host = input("FortiManager IP: ")
+    if not host:
+        print("FortiManager IP cannot be empty. Please enter a valid IP.")
+
+### Objectname
+objectname = ""
+while not objectname:
+    objectname = input("Object name: ")
+    if not objectname:
+        print("Object name cannot be empty. Please enter a valid name.")
+
+### Associated interface
+associated_interface = ""
+while not associated_interface:
+    associated_interface = input("Interface (type any for default): ")
+    if not associated_interface:
+        print("Interface cannot be empty. Please enter a valid interface name.")
+
+### Subnet
+subnet = ""
+while not subnet:
+    subnet = input("IP address / netmask: ")
+    if not subnet:
+        print("IP address / netmask cannot be empty. Please enter a valid value.")
 
 ## Assemble FortiManager connection details
 FortiManager = pyFortiManagerAPI.FortiManager(
