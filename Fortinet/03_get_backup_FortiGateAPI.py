1d06ba6f0959f850cfaedd4041b27cdf1ee63155
joshbergm <josh.bergman@outlook.com>
2024-03-22 11:37:37 +0100

    changed request

--- Fortinet/03_get_backup_FortiGateAPI.py
+++ Fortinet/03_get_backup_FortiGateAPI.py
@@ -9,11 +9,14 @@ from fortigate_api import FortiGateAPI
 import getpass
 from datetime import datetime
 import os
+import json
+import requests
 
 ## Define login variables
 fortigate_ip = input("FortiGate IP: ")
 username = input("Username: ")
 token = getpass.getpass(prompt="Token: ", stream=None)
+vdom = 'root'
 
 ## Define other
 pc_username = getpass.getuser()
@@ -21,10 +24,10 @@ date = datetime.now().strftime('%d_%m_%Y')
 backup_file_path = os.path.join("c:/Users/", pc_username, "Documents/Python-Networking/")
 
 ## Assemble API request
-fortigate_backup_api = FortiGateAPI(host=fortigate_ip, token=token)
-backup_api_request = f'https://{fortigate_ip}/api/v2/monitor/system/config/backup?'
-
-backup_file_response = fortigate_backup_api.fortigate.get(backup_api_request)
+client = requests.session()
+client.verify = False
+backup_api_request =(f'https://{fortigate_ip}/api/v2/monitor/system/config/backup?scope=vdom&vdom={vdom}&access_token={token}')
+backup_file_response = client.get(backup_api_request)
 
 ## Write backup content to file
 if backup_file_response.ok:
